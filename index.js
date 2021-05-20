d62149e8e1f75005992947e7ea8d996284cdbc0a
himanshuchawla009 <himanshuchawla2014@gmail.com>
2021-05-20 11:37:26 +0530

    standard abis added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import * as Erc1155 from './abis/ERC1155.json';
-import * as ERC1155Meta from './abis/ERC1155Meta.json';
-import * as ERC1155Metadata from './abis/ERC1155Metadata.json';
-import * as ERC1155MintBurn from './abis/ERC1155MintBurn.json';
+const ERC1155 =  require('./abis/ERC1155.json');
+const ERC1155Meta = require('./abis/ERC1155Meta.json');
+const ERC1155Metadata = require('./abis/ERC1155Metadata.json');
+const ERC1155MintBurn = require('./abis/ERC1155MintBurn.json');
 
-export {
-  Erc1155,
+module.exports = {
+  ERC1155,
   ERC1155Meta,
   ERC1155Metadata,
   ERC1155MintBurn
